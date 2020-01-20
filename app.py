a770da029634f3a76a41c1024e9e0cb850e279eb
Your Name <divineayomi@gmail.com>
2020-01-20 17:15:19 +0100

    made ajax submit to api

--- app.py
+++ app.py
@@ -3,17 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+from flask_cors import CORS
+
+CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL = 'mysql+pymysql://erioluwa:erioluwa@127.0.0.1/erioluwa'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+class todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo = db.Column(db.String(32))
+
+db.create_all()
 
 @app.route("/index" , methods= ['GET'])
 def index():
     return render_template("index.html")
 
-# @app.route("/", methods=['POST'])
-# def add():
+@app.route("/", methods=['POST'])
+def add():
+    todo = request.json.get("todo")
+    newTodo = todo(todo=todo)
+    db.session.add(newTodo) 
+    db.session.commit()
     
 if __name__ == "__main__":
     app.run(debug=True)
