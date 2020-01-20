79ae19a762f7e3f4053c8bf3acea36d0eddcedc6
Your Name <divineayomi@gmail.com>
2020-01-20 16:54:15 +0100

    user interface

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index" , methods= ['get'])
 def index():
-    return ('hello world!')
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
